6c22d594b0da6f695d5fb907b7d31df8a085063d
ks <55551518+kevsoto01@users.noreply.github.com>
2026-07-09 22:55:27 -0400

    -

--- pepper_v2_app/trash/old/conversation.py
+++
@@ -1,8 +0,0 @@
-def conversation(user_text: str, assistant_text: str, history):
-    
-    user_msg = {'role': 'user', 'content': user_text}
-    assistant_msg = {'role': 'assistant', 'content': assistant_text}
-    history.append(user_msg)
-    history.append(assistant_msg)
-    
-    return history
--- pepper_v2_app/trash/old/file_io.py
+++
@@ -1,7 +0,0 @@
-from ..utils.paths import LOCAL_PEPPER_AUDIO, REMOTE_PEPPER_AUDIO
-
-def open_sftp(ssh):
-    return ssh.open_sftp()
-
-def upload_audio(sftp, local_file=LOCAL_PEPPER_AUDIO, remote_file=REMOTE_PEPPER_AUDIO):
-    sftp.put(str(local_file), remote_file)
