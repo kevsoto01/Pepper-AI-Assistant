7d61c1c821938174e6ed475d5da9eae2cea73da5
ks <55551518+kevsoto01@users.noreply.github.com>
2026-07-09 22:59:30 -0400

    -

--- pepper_v2_app/tests/chatbot_test.py
+++
@@ -1,8 +0,0 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  6 17:44:28 2026
-
-@author: kevso
-"""
-
--- pepper_v2_app/tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-import numpy 
-
-def spam_sqrt(maxx):
-    for itemer in range(1, maxx):
-        print(numpy.sqrt(itemer))
-    
-        
